82b4878c87f83667c31a235f14cab8956974325e
Ammar Al-Khawaldeh <me@ammar.dev>
2022-06-02 00:11:58 +0300

    Define products table

--- database/migrations/2022_06_01_210113_create_products_table.php
+++ database/migrations/2022_06_01_210113_create_products_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('price')->comment('Price in cents');
+            $table->foreignId('store_id')->constrained();
             $table->timestamps();
         });
     }
