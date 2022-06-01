79ca8469d918220ef5c3c40273f3ce14dac86fe5
Ammar Al-Khawaldeh <me@ammar.dev>
2022-06-02 00:41:34 +0300

    Use user creator action in the controller

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\CreateNewUser;
 use Illuminate\Http\Request;
 
 class AuthenticationController extends Controller
 {
-    //
+    public function login()
+    {
+        # code...
+    }
+    
+    public function register(Request $request)
+    {
+        $user = (new CreateNewUser())->create($request->all());
+        return response()->json($user, 201);
+    }
 }
