c8adca8bdecc13d851cfa5a41266841d958e960c
Ammar Al-Khawaldeh <me@ammar.dev>
2022-06-02 00:09:29 +0300

    Define stores table

--- database/migrations/2022_06_01_210053_create_stores_table.php
+++ database/migrations/2022_06_01_210053_create_stores_table.php
@@ -15,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('stores', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+
+            $table->boolean('is_vat_included')->default(true);
+            $table->integer('vat_percentage')->default(0);
+
+            $table->integer('shipping_fixed_cost')->default(0);
+
+            $table->unsignedBigInteger('owner_id');
+            $table->foreign('owner_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
