a2e0777aa5fb55f26e803cfccc144fd396856981
Ammar Al-Khawaldeh <me@ammar.dev>
2022-06-03 01:55:53 +0300

    Implement product deletion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,6 +31,7 @@ class ProductController extends Controller
 
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return response('', 204);
     }
 }
