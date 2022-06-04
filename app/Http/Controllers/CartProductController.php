13174458c8c4f51fa75fc18f7df8091bd21aa603
Ammar Al-Khawaldeh <me@ammar.dev>
2022-06-04 18:05:57 +0300

    Store products to cart

--- app/Http/Controllers/CartProductController.php
+++ app/Http/Controllers/CartProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\CartProductRequest;
+use App\Models\Product;
 
 class CartProductController extends Controller
 {
@@ -14,5 +15,19 @@ class CartProductController extends Controller
     public function index()
     {
         return auth()->user()->products()->withPivot('quantity')->get();
+    }
+
+    public function store(CartProductRequest $request)
+    {
+        $currentStoreId = $request->user()->products()->select('store_id')->first()?->store_id;
+        $product = Product::select('store_id')->firstWhere('id', $request->product_id);
+
+        if ($currentStoreId !== null && $currentStoreId !== $product->store_id) {
+            return response()->json(['message' => 'You can\'t add products from multiple stores'], 422);
+        }
+
+        $request->user()->products()->syncWithoutDetaching($request->product_id, ['quantity' => $request->quantity]);
+
+        return response()->json(['message' => 'Product added to cart']);
     }
 }
